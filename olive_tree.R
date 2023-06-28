2a0cffb30ac48b94030c31a6ff7dc94dfc278b9a
Jason Charamis <125705059+JasonCharamis@users.noreply.github.com>
2023-06-28 14:46:18 +0300

    Update olive_tree.R

--- olive_tree.R
+++ olive_tree.R
@@ -1,49 +1,54 @@
 
 ## Library of functions for advanced tree manipulation and visualization using ggtree.
 
-## load dependencies
-## tree manipulation and visualization related
-library("ape")
-library("phytools")
-library("treeio")
-library("TreeTools")
-library("ggstar")
-library("ggtree")
+package_install <- function ( package ) {
+  if (requireNamespace(package, quietly = TRUE)) {
+      library(package, character.only = TRUE)
+  }
+  
+  else {
+    print ( (sprintf("%s %s",package, "is not installed. Installing it!")))
+    BiocManager::install(package)  
+  }
+}
 
-# regex related
-library("dplyr")
-library("stringi")
-library("stringr")
+
+## load dependencies - if not present will install them
+dependencies <- c("ape","phytools","treeio","TreeTools","ggstar","ggtree","ggplot2","dplyr","stringi","stringr")
+
+for ( i in dependencies ) { 
+  package_install(i)
+  }
 
 
 ## read and order tree 
 read_tree <- function ( nwk ) {
-    tree <- ape::read.tree(nwk)
-    t <- Preorder(tree)
-    return (t)
+  tree <- ape::read.tree(nwk)
+  t <- Preorder(tree)
+  return (t)
 }
 
 
 ## print tree with node ids - very useful for multiple tasks
 node_ids <- function ( tree ) {
-    ggtree(tree, layout="circular") + geom_text(aes(label = node), size = 3)
+  ggtree(tree, layout="circular") + geom_text(aes(label = node), size = 3)
 }
 
 ## collapse nodes based on bootstrap support - returns a "phylo" object
 bootstrap_collapse <- function ( tree, cutoff ) { 
-    return ( as.polytomy(tree, feature='node.label', fun=function(x) as.numeric(x) < cutoff) )
+  return ( as.polytomy(tree, feature='node.label', fun=function(x) as.numeric(x) < cutoff) )
   }
 
 
 #flip based on descendant nodes (internal nodes or leaves if node is terminal)
 flip_node <- function (tree, node1, node2) {
-    flipped_tree <- flip(ggtree(tree), node1, node2)
-    return ( as.phylo(flipped_tree) )
+      flipped_tree <- flip(ggtree(tree), node1, node2)
+      return (as.phylo(flipped_tree))
 }
 
 
 ## group all descendant branches of node(s)
-group_descendants <- function (tree, node1, node2=NULL, node3=NULL, node4=NULL) {
+group_descendants <- function (tree, node1, node2="", node3="",node4="") {
     tree <- groupClade(tree, .node=c(node1, node2, node3, node4))
     return ( tree )
 }
@@ -51,8 +56,8 @@ group_descendants <- function (tree, node1, node2=NULL, node3=NULL, node4=NULL) 
 
 ## extract subtree by finding the MRCA of two anchor nodes while preserving branch lengths and bootstrap values 
 extract_subtree <- function (t, tip1, tip2) {
-    subtree <- ape::extract.clade(t,node = phytools::findMRCA(t,c(tip1, tip2)))
-    return ( subtree )
+  subtree <- ape::extract.clade(t,node = phytools::findMRCA(t,c(tip1, tip2)))
+  return ( subtree )
 }
 
 
@@ -72,7 +77,7 @@ bootstrap_circles <- function ( x ) {
 }
 
 ## visualize tree with a wide variety of options and customizable features
-draw_single_tree <- function ( tree, node1=NULL, node2=NULL, reference1=NULL, reference2=NULL, bootstrap_legend=TRUE, ... ) {
+draw_single_tree <- function ( tree, node1=NULL, node2=NULL, node3=NULL, reference1=NULL, reference2=NULL, bootstrap_legend=TRUE, ... ) {
   
   ## if species matches reference discard from rest of tip labels - at least two references, adjust for more
   if (!is.null(reference2)) {
@@ -127,7 +132,8 @@ draw_single_tree <- function ( tree, node1=NULL, node2=NULL, reference1=NULL, re
                        perniciosus="15",
                        schwetzi="10",
                        sergenti="15",
-                       tobbi="15")
+                       tobbi="15"
+  )
   
   
   ## create associative dataframes of tip color and shape 
@@ -146,10 +152,11 @@ draw_single_tree <- function ( tree, node1=NULL, node2=NULL, reference1=NULL, re
   bootstrap_colors <- c('(75,100]' = "black",
                         '(50,75]' = "grey",
                         '(0,50]' = "snow2")
-
-  root <- rootnode(tree)
-
-  ## Decide if you want to print bootstrap legend or not
+  
+  node_ids(tree) ## print the node ids to select which nodes to highlight
+  
+  nodes <- as.character(x@data$node)
+  
   if (bootstrap_legend == TRUE) {
       bootstrap_legend = 'legend'
   }
@@ -158,16 +165,36 @@ draw_single_tree <- function ( tree, node1=NULL, node2=NULL, reference1=NULL, re
       bootstrap_legend = "none"
   }
   
-  nodes <- as.character(x@data$node)
+  if ( !is.null(node3) ) {
+    highlights <- c("olivedrab2","rosybrown1","wheat1")
+  }
   
+  else {
+    highlights <- c("rosybrown1","wheat1")
+  }
+  
+  root <- rootnode(tree)
   ## draw tree with bootstrap nodes, color and shape mappings, and highlighted nodes
   ggtree(x) + geom_star(aes(x,subset=isTip,starshape=shape, fill=species.x), size=3, show.legend = F) +
     geom_point2(aes(subset=!isTip & node != root, fill=category), shape=21, size=2) + theme_tree(legend.position=c(0.8, 0.2)) +
     scale_fill_manual(values=c(group_colors,bootstrap_colors), guide=bootstrap_legend, name='Bootstrap Support (BS)',
                       breaks=c('(75,100]', '(50,75]', '(0,50]'), labels=expression("BS >=75", "50 <= BS < 75", "BS < 50")) +
-    geom_highlight(node=c( node1, node2), fill=c("rosybrown1","wheat1"), alpha=0.3, extend=0.10, linetype=23, colour="black") +
+    geom_highlight(node=c( node1, node2, node3), fill=highlights, alpha=0.3, extend=0.10, linetype=23, colour="black") +
     geom_text(aes(label = ifelse(nodes %in% ref_species$node, ref_species$name, "")), 
               position = position_nudge(x = 0.08), vjust = 0.5, hjust=0.9, size = 3.5, family="Arial", fontface="bold")
+}
+
+
+draw_pairs <- function (fig1, fig2, horizontal = F, ...) {
+  package_install("gridExtra")
+  
+  if ( horizontal == T) {
+    multi_panel_figure <- grid.arrange( fig1, fig2, nrow = 2)
+  }
+  
+  else {
+    multi_panel_figure <- grid.arrange(fig1, fig2, nrow = 1)
+  }
 }
 
 
